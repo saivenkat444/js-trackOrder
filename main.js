25adcc9b0ccf47ee456169727a9109cbd3630a44
saiVenkat <kamanasaivenkat444@gmail.com>
2025-01-31 12:25:22 +0530

    created an expectation folder

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const main = () => {
-  
+  const {}
   //place order and have the order id and details
   //food prep detail
   //packing details
@@ -7,11 +7,10 @@ const main = () => {
   //display the summary of the order 
 }
 
-//lets have a structure that tells me the order id in the order placement
+//lets have a structure that tells me the order id when order placed (can be a number) 
 //food preparaion should tell me the food itself (it can be an array)
-//pack the food and tell me the packaging details
-//delivery details should be added at last 
-
+//pack the food and tell me the packaging details(can be a string)
+//delivery details should be added at last (can be object {name, address, time})
 //want to handle them separate and then combine together to get the data log 
 //get the start time first, then for every process calculate the time elapsed 
 //for the whole process just add the times elapsed for the processes.
