7acd5e17ce67130d225654c3688a3eb562f05fc9
saiVenkat <kamanasaivenkat444@gmail.com>
2025-01-31 12:13:51 +0530

    idea is added

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
+const main = () => {
+  
+  //place order and have the order id and details
+  //food prep detail
+  //packing details
+  //dlivery details
+  //display the summary of the order 
+}
 
+//lets have a structure that tells me the order id in the order placement
+//food preparaion should tell me the food itself (it can be an array)
+//pack the food and tell me the packaging details
+//delivery details should be added at last 
+
+//want to handle them separate and then combine together to get the data log 
+//get the start time first, then for every process calculate the time elapsed 
+//for the whole process just add the times elapsed for the processes.
